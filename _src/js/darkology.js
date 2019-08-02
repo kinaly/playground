d153b12aa8e08345ec5270301f86634010b0fe6b
Kinaly VENEVONGSOS <hello@kinaly.net>
2019-08-02 17:32:58 +0100

    Set up automatic styling of the grid for rows

--- _src/js/darkology.js
+++ _src/js/darkology.js
@@ -27,6 +27,8 @@ function gridSetup(element, targetEpisode) {
 
 			const charactersOrderList = data.charactersOrder;
 
+			const periods = data.darkPeriods;
+
 			// Grab latest episode if targetEpisode is unspecified
 			let episode = targetEpisode ? targetEpisode : charactersOrderList[charactersOrderList.length - 1].episode;
 
@@ -45,6 +47,20 @@ function gridSetup(element, targetEpisode) {
 				}
 			}
 
-			headStyles.innerHTML = ".map { grid-template-columns: " + templateColumnSetup + " }";
+			let templateRowSetup = "";
+
+			let knownPeriods = periods.filter(item => item.episode <= episode);
+
+			for (var i = 0; i < knownPeriods.length; i++) {
+				if (i == 0) {
+					templateRowSetup = templateRowSetup + "[full-start time" + knownPeriods[i].id + "-start header" + knownPeriods[i].id + "-start] auto [header" + knownPeriods[i].id + "-end old" + knownPeriods[i].id + "-start] auto [old" + knownPeriods[i].id + "-end adult" + knownPeriods[i].id + "-start] auto [adult" + knownPeriods[i].id + "-end young" + knownPeriods[i].id + "-start] auto [young" + knownPeriods[i].id + "-end time" + knownPeriods[i].id + "-end ";
+				} else if (i > 0 && i < (knownPeriods.length - 1)) {
+					templateRowSetup = templateRowSetup + "time" + knownPeriods[i].id + "-start header" + knownPeriods[i].id + "-start] auto [header" + knownPeriods[i].id + "-end old" + knownPeriods[i].id + "-start] auto [old" + knownPeriods[i].id + "-end adult" + knownPeriods[i].id + "-start] auto [adult" + knownPeriods[i].id + "-end young" + knownPeriods[i].id + "-start] auto [young" + knownPeriods[i].id + "-end time" + knownPeriods[i].id + "-end ";
+				} else {
+					templateRowSetup = templateRowSetup + "time" + knownPeriods[i].id + "-start header" + knownPeriods[i].id + "-start] auto [header" + knownPeriods[i].id + "-end old" + knownPeriods[i].id + "-start] auto [old" + knownPeriods[i].id + "-end adult" + knownPeriods[i].id + "-start] auto [adult" + knownPeriods[i].id + "-end young" + knownPeriods[i].id + "-start] auto [young" + knownPeriods[i].id + "-end time" + knownPeriods[i].id + "-end full-end]";
+				}
+			}
+
+			headStyles.innerHTML = ".map { grid-template-columns: " + templateColumnSetup + "; grid-template-rows: " + templateRowSetup + "; }";
 		})
 }
